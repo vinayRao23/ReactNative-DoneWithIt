20e449bb1410a606b7b76394f48497d63c051bd5
CoolKid <vinay23.rao@gmail.com>
2021-03-22 19:45:02 -0700

    Implemented custom AppText component.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Text, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
+import AppText from "./app/components/AppText";
+
 const App = () => {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppText>I love React Native!</AppText>
+    </View>
+  );
 };
 
 export default App;
